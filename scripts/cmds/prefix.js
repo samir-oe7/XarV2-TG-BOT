d63e98b704d15ca3fb5086f5a869f69167c8793b
Samir Œ <163100703+samirxpikachuio@users.noreply.github.com>
2024-05-23 18:22:59 +0000

    XarV2-TG-BOT RELEASE

---
+++ scripts/cmds/prefix.js
@@ -0,0 +1,16 @@
+const config = require('../../config.json');
+
+module.exports = {
+    config: {
+        name: "prefix",
+        author: "Samir Œ",
+        description: "bot prefix",
+        category: "utility",
+        usage: "prefix",
+        usePrefix: false
+    },
+    onStart: async function ({ bot, chatId }) {
+        bot.sendMessage(chatId, `🌐 System prefix: ${config.prefix}`);
+    }
+};
+
